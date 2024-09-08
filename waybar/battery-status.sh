f3a5cf36d54006baa66ee599f626976cde1bef37
Alex Wyatt <alexander@jdwyatt.com>
2024-09-08 19:53:27 -0400

    updated waybar

--- waybar/battery-status.sh
+++ waybar/battery-status.sh
@@ -9,8 +9,6 @@ BATTERY_LEVEL=$(cat "$BATTERY_FILE")
 # Check if the battery level is below the threshold
 if [ "$BATTERY_LEVEL" -le "10" ]; then
     notify-send "Low Battery ($BATTERY_LEVEL%)" "fucking charge the laptop"
-#elif [ "$BATTERY_LEVEL" -le "10" ]; then
-#    notify-send "Battery is low" "iewhCurrent level: $BATTERY_LEVEL%"
 elif [ "$BATTERY_LEVEL" -le "20" ]; then
     notify-send "Low Battery ($BATTERY_LEVEL%)" "Plug it in dumbass"
 fi
