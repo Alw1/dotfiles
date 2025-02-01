99f47bf70f786188d26dd1be20dffffcc5b5bedf
Alex Wyatt <alexander@jdwyatt.com>
2025-01-31 20:42:56 -0500

    Updated dotfiles to include hyprland, nvim, and fuzzel

---
+++ config/hypr/screenshot.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+MODE="$1"  
+SCREENSHOT_DIR=~/Pictures/Screenshots
+NAME="screenshot-$(date +%Y%m%d-%H%M%S).png"
+
+mkdir -p "$SCREENSHOT_DIR"
+
+if [ "$MODE" = "window" ]; then  
+    grim $SCREENSHOT_DIR/$NAME && \
+    exec notify-send -i $SCREENSHOT_DIR/$NAME "Screenshot saved"
+elif [ "$MODE" = "selection" ]; then  
+    grim -g "$(slurp)" $SCREENSHOT_DIR/$NAME \
+    && exec notify-send -i $SCREENSHOT_DIR/$NAME "Screenshot saved"
+fi
