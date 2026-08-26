633bf21728ee0d0ca3bbf69b577f884acf0b4192
Alex Wyatt <alexander@jdwyatt.com>
2026-08-26 12:57:59 -0400

    Added neovim config as a git submodule

--- nvim/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("core")
-
--- Set theme to the theme of the last session
-local ok, last_color = pcall(require, 'last-color')
-local theme = ok and last_color.recall() or 'habamax'
-pcall(vim.cmd.colorscheme, theme)
--- nvim/.config/nvim/lua/core/init.lua
+++
@@ -1,7 +0,0 @@
-require("core.commands")
-require("core.options")
-require("core.keymaps")
-require("core.lazy")
-
-
-
